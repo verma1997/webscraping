61273c66e2fc159b2393c31b11c6b5512246b148
Priyanshu Verma <201652019@iiitvadodara.ac.in>
2019-06-19 11:46:45 +0530

    initial commit

---
+++ webscraping/items.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class WebscrapingItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    
+    offer_title = scrapy.Field()
+    offer_coupon = scrapy.Field()
+    offer_validity = scrapy.Field()
+    offer_image = scrapy.Field()
+   
+    pass
---
+++ webscraping/spiders/yatra_offers.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from ..items import WebscrapingItem
+
+
+class YatraOffersSpider(scrapy.Spider):
+    name = 'yatra_offers'
+    start_urls = [
+    	'https://www.yatra.com/offer/dom/listing/domestic-flight-deals',
+    ]
+
+    next_pages = [
+		'https://www.yatra.com/offer/dom/listing/international-flight-deals',
+		'https://www.yatra.com/offer/dom/listing/domestic-hotel-deals',
+		'https://www.yatra.com/offer/dom/listing/holiday-deals',
+		'https://www.yatra.com/offer/listing/bus',
+		'https://www.yatra.com/offer/listing/activity-deals'
+	]
+    
+    total_pages = 0
+
+    def parse(self, response):
+    	items = WebscrapingItem()
+
+    	all_li_offers = response.css('ul.wfull')
+
+    	for offers in all_li_offers:
+    		offer_title = offers.css('.offerMainTitle::text').extract()
+    		offer_coupon = offers.css('.mt5::text').extract()
+    		offer_validity = offers.css('.date::text').extract()
+    		offer_image = offers.css('.respnsiv-img::attr(src)').extract()
+
+    		items['offer_title'] = offer_title
+	    	items['offer_coupon'] = offer_coupon
+	    	items['offer_validity'] = offer_validity
+	    	items['offer_image'] = offer_image
+
+	    	yield items
+
+	    	if YatraOffersSpider.total_pages < 5:
+	    		YatraOffersSpider.total_pages += 1
+	    		yield response.follow(YatraOffersSpider.next_pages[YatraOffersSpider.total_pages], callback = self.parse)
